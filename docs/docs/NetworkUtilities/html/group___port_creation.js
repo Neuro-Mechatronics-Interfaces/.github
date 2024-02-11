a5e1b2c4e16e2697452c3dfed8719542795d48e1
Max (Lenovo) <max.murphy11@gmail.com>
2024-02-11 17:54:53 -0500

    Refresh docs for NetworkUtilities again

--- docs/docs/NetworkUtilities/html/group___port_creation.js
+++ docs/docs/NetworkUtilities/html/group___port_creation.js
@@ -1,6 +1,7 @@
 var group___port_creation =
 [
-    [ "initializeUDPsock", "group___port_creation.html#ga74bfcf0321b2813cb8fe23273c9dae3b", null ],
+    [ "initializeUDPsock", "group___port_creation.html#ga9c26022fa61448e4f95aa9837016a749", null ],
     [ "initializeWinsock", "group___port_creation.html#ga5ee93bf6ea9d1f37eedb43e05ebf0157", null ],
+    [ "setBlocking", "group___port_creation.html#ga26cae777cee8ec996251dab63cb40da7", null ],
     [ "setNonBlocking", "group___port_creation.html#ga3eb74aed34ee71625a337a5fa9a4f5ba", null ]
 ];
--- docs/docs/NetworkUtilities/html/network_8c.js
+++ docs/docs/NetworkUtilities/html/network_8c.js
@@ -1,9 +1,10 @@
 var network_8c =
 [
     [ "cleanupWinsock", "group___clean_up.html#ga1af42a80836683ce46af20b6768565bb", null ],
-    [ "initializeUDPsock", "group___port_creation.html#ga74bfcf0321b2813cb8fe23273c9dae3b", null ],
+    [ "initializeUDPsock", "group___port_creation.html#ga9c26022fa61448e4f95aa9837016a749", null ],
     [ "initializeWinsock", "group___port_creation.html#ga5ee93bf6ea9d1f37eedb43e05ebf0157", null ],
     [ "sendCursorPosition", "group___messaging.html#ga7d21378b8f3bd51fd2157930de9b139f", null ],
     [ "sendTargetUpdate", "group___messaging.html#ga00dd7fbc289ed250870a37fb306a6080", null ],
+    [ "setBlocking", "group___port_creation.html#ga26cae777cee8ec996251dab63cb40da7", null ],
     [ "setNonBlocking", "group___port_creation.html#ga3eb74aed34ee71625a337a5fa9a4f5ba", null ]
 ];
