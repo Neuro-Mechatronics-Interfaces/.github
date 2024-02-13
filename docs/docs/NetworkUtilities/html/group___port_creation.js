03c872c9dba193e112bf1209395b2b295ce49481
Max-MI115 <max.murphy11@gmail.com>
2024-02-13 13:25:26 -0500

    Update NetworkUtilities docs
    Again update NetworkUtilities docs

--- docs/docs/NetworkUtilities/html/group___port_creation.js
+++ docs/docs/NetworkUtilities/html/group___port_creation.js
@@ -1,6 +1,8 @@
 var group___port_creation =
 [
-    [ "initializeUDPsock", "group___port_creation.html#ga74bfcf0321b2813cb8fe23273c9dae3b", null ],
+    [ "initializeUDPClientSocket", "group___port_creation.html#gaca987f2e3c79bcd8634284953ab9a28d", null ],
+    [ "initializeUDPServerSocket", "group___port_creation.html#ga10261707629d43e8de5c81d115292c1b", null ],
     [ "initializeWinsock", "group___port_creation.html#ga5ee93bf6ea9d1f37eedb43e05ebf0157", null ],
+    [ "setBlocking", "group___port_creation.html#ga26cae777cee8ec996251dab63cb40da7", null ],
     [ "setNonBlocking", "group___port_creation.html#ga3eb74aed34ee71625a337a5fa9a4f5ba", null ]
 ];
